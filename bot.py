8b4b578ede4af75f507abfd54b1440fba55fd3ee
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-02-02 14:16:04 +0300

    add webhook

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
+from asyncore import write
 from flask import Flask
+from flask import request
+from flask import jsonify
 import os
 import requests
 from dotenv import load_dotenv
@@ -10,7 +13,7 @@ load_dotenv()
 TG_TOKEN = os.getenv('TG_TOKEN')
 URL = f'https://api.telegram.org/bot{TG_TOKEN}/'
 
-# app = Flask(__name__)
+ 
 # sslify = SSLify(app)
 
 def write_json(data, filename='answer'):
@@ -32,9 +35,23 @@ def send_messages(chat_id, text='Привет'):
 def main():
     r = requests.get(URL + 'getMe')
     write_json(r.json())
+    pass
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        r = request.get_json()
+        write_json(r)
+        return jsonify(r)
+    return '<h1> Hi, i am work. </h1>'
 
 
 if __name__ == '__main__':
-    r = get_updates()
-    chat_id = r['result'][-1]['message']['chat']['id']
-    send_messages(chat_id)
+    # r = get_updates()
+    # chat_id = r['result'][-1]['message']['chat']['id']
+    # send_messages(chat_id)
+    app.run()
+
+webhook = f'https://api.telegram.org/bot{TG_TOKEN}/setWebhook?url=https://dfb5-62-217-186-124.ngrok.io/'
