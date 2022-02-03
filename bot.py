cd2bf54701ee369cee977451ab6cb3f109acd45a
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-02-03 12:47:18 +0300

    add API Yahoofinace

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-from asyncore import write
 from flask import Flask
 from flask import request
 from flask import jsonify
@@ -8,23 +7,17 @@ from dotenv import load_dotenv
 import json
 
 # from flask_sslify import SSLify
+# sslify = SSLify(app)
 
 load_dotenv()
 TG_TOKEN = os.getenv('TG_TOKEN')
 URL = f'https://api.telegram.org/bot{TG_TOKEN}/'
 
- 
-# sslify = SSLify(app)
 
 def write_json(data, filename='answer'):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
 
-def get_updates():
-    url = URL + 'getUpdates'
-    r= requests.get(url)
-    # write_json(r.json())
-    return r.json()
 
 def send_messages(chat_id, text='Привет'):
     url = URL + 'sendMessage'
@@ -32,26 +25,26 @@ def send_messages(chat_id, text='Привет'):
     r = requests.post(url, json=answer)
     return r.json()
 
-def main():
-    r = requests.get(URL + 'getMe')
-    write_json(r.json())
-    pass
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
+        print('lol')
         r = request.get_json()
+        chat_id = r['message']['chat']['id']
+        message =  r['message']['text']
+
+        if 'sber' in message.lower():
+            print('sber')
+            send_messages(chat_id, text='цена сбера')
         write_json(r)
         return jsonify(r)
     return '<h1> Hi, i am work. </h1>'
 
 
 if __name__ == '__main__':
-    # r = get_updates()
-    # chat_id = r['result'][-1]['message']['chat']['id']
-    # send_messages(chat_id)
     app.run()
 
-webhook = f'https://api.telegram.org/bot{TG_TOKEN}/setWebhook?url=https://dfb5-62-217-186-124.ngrok.io/'
+webhook = f'https://api.telegram.org/bot{TG_TOKEN}/setWebhook?url=https://7ae5-62-217-186-124.ngrok.io'
---
+++ request_price_stock.py
@@ -0,0 +1,39 @@
+import yfinance as yf
+
+all_rus_stocks = ['SBER.ME', 'GAZP.ME', 'LKOH.ME', 'YANDX.ME', 'GMKN.ME', 'MTSS.ME', 'ALRS.ME', 'NLMK.ME', 'MAGN.ME']
+
+def get_stock_info(stock_ticker):
+    stock = yf.Ticker(stock_ticker)
+    sector = stock.info['sector']
+    price = stock.info['regularMarketPrice']
+    last_divident = stock.info['lastDividendValue']
+    day_50 = stock.info['fiftyDayAverage']
+    open_price = stock.info['open']
+    marketCap = stock.info['marketCap']
+    daylow = stock.info['dayLow']
+    dayhigh = stock.info['dayHigh']
+    hi_52 = stock.info['fiftyTwoWeekHigh']
+    middle_52 = stock.info['fiftyDayAverage']
+    low_52 = stock.info['fiftyTwoWeekLow']
+    divident = stock.info['dividendRate']
+    last_price_close = stock.info['previousClose']
+    procent = round((price - last_price_close)/ price * 100, 2)
+    result = {
+        'сектор' : sector,
+        'капитализация' : marketCap,
+        'цена' : price,
+        'процент изменения' : procent,
+        'цена последнего закрытия' : last_price_close,
+        'цена открытия денвной сессии' : open_price,
+        'дневной максимум' : dayhigh,
+        'дневной минимум' : daylow,
+        '50 дневная средняя' : day_50,
+        '52 дневный максимум' : hi_52,
+        '52 дневная средняя' : middle_52,
+        '52 дневный минимум' : low_52,
+        'Размер последнего дивиденда на одну акцию' : last_divident,
+        'Размер дивиденда на одну акцию' : divident,
+        }
+    return result
+
+print(get_stock_info('SBER.ME'))
